9ef51b63df7ff4f2ccb79abf4d1d917813436a7d
Nelson Miranda <nelsonmiranda8421@gmail.com>
2020-06-03 23:00:55 -0600

    Creating RestController

--- src/main/java/com/miranda/springboot/springbootquestionary/SpringbootQuestionaryApplication.java
+++ src/main/java/com/miranda/springboot/springbootquestionary/SpringbootQuestionaryApplication.java
@@ -2,10 +2,8 @@ package com.miranda.springboot.springbootquestionary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.miranda.springboot")
 public class SpringbootQuestionaryApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/miranda/springboot/springbootquestionary/WelcomeService.java
+++ src/main/java/com/miranda/springboot/springbootquestionary/WelcomeService.java
@@ -1,4 +1,4 @@
-package com.miranda.springboot.server;
+package com.miranda.springboot.springbootquestionary;
 
 import org.springframework.stereotype.Component;
 
