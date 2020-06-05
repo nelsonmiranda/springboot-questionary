e5fdfde47d4a2e0d70e00acf6bef281c0f3fc82c
Nelson Miranda <nelsonmiranda8421@gmail.com>
2020-06-04 22:54:58 -0600

    Advance application configuration - Type safe configuration properties

--- src/main/java/com/miranda/springboot/springbootquestionary/WelcomeController.java
+++ src/main/java/com/miranda/springboot/springbootquestionary/WelcomeController.java
@@ -1,15 +1,23 @@
 package com.miranda.springboot.springbootquestionary;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.miranda.springboot.springbootquestionary.configuration.BasicConfiguration;
 
 @RestController
 public class WelcomeController {
 
 	@Autowired
 	private WelcomeService welcomeService;
+	
+	@Autowired
+	private BasicConfiguration configuration;
 	
 	@Value("${welcome.message}")
 	private String welcomeMessage;
@@ -18,4 +26,16 @@ public class WelcomeController {
 	public String welcome() {
 		return welcomeMessage;
 	}
+	
+	@RequestMapping("/dynamic-configuration")
+	public Map dynamicConfiguration(){
+		Map map = new HashMap();
+		
+		map.put("value", configuration.isValue());
+		map.put("message", configuration.getMessage());
+		map.put("number", configuration.getNumber());
+		
+		return map;
+	}
+	
 }
