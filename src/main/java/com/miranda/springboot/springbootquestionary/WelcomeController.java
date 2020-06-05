90a6b5893326bc002d42b842c94d377a4d312df7
Nelson Miranda <nelsonmiranda8421@gmail.com>
2020-06-04 21:39:58 -0600

    Customize Welcome Message

--- src/main/java/com/miranda/springboot/springbootquestionary/WelcomeController.java
+++ src/main/java/com/miranda/springboot/springbootquestionary/WelcomeController.java
@@ -1,6 +1,7 @@
 package com.miranda.springboot.springbootquestionary;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,11 @@ public class WelcomeController {
 	@Autowired
 	private WelcomeService welcomeService;
 	
+	@Value("${welcome.message}")
+	private String welcomeMessage;
+	
 	@RequestMapping("/welcome")
 	public String welcome() {
-		return welcomeService.retrieveWelcomeMessage();
+		return welcomeMessage;
 	}
 }
