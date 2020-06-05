0e4689c9b6f3537e4658c5ac6fc98e2799cb2b96
Nelson Miranda <nelsonmiranda8421@gmail.com>
2020-06-05 12:37:23 -0600

    Added entities with commandLineRunner and h2 database url updated

--- src/main/java/com/miranda/springboot/springbootquestionary/helpers/UserCommandLineRunner.java
+++ src/main/java/com/miranda/springboot/springbootquestionary/helpers/UserCommandLineRunner.java
@@ -1,14 +1,37 @@
 package com.miranda.springboot.springbootquestionary.helpers;
 
+import java.util.Iterator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import com.miranda.springboot.springbootquestionary.entity.User;
+import com.miranda.springboot.springbootquestionary.repository.UserRepository;
+
+
 
 @Component
 public class UserCommandLineRunner implements CommandLineRunner {
 
+	private static final Logger log =  LoggerFactory.getLogger(UserCommandLineRunner.class);
+	
+	@Autowired
+	private UserRepository userRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("UserCommandLineRunner");
+		userRepository.save(new User("Admin", "Admin"));
+		userRepository.save(new User("Ranga", "Admin"));
+		userRepository.save(new User("Ravi", "User"));
+		userRepository.save(new User("Satish", "Admin"));
+		userRepository.save(new User("Raghu", "User"));
+
+		for (User user : userRepository.findAll()) {
+			log.info(user.toString());
+		}
 	}
 
 }
