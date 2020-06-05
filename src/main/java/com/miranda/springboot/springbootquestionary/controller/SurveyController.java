f90276be16a565b1cf6cdfee41d81ea0c81ce8cd
Nelson Miranda <nelsonmiranda8421@gmail.com>
2020-06-04 19:08:11 -0600

    REST service to add question to survey

--- src/main/java/com/miranda/springboot/springbootquestionary/controller/SurveyController.java
+++ src/main/java/com/miranda/springboot/springbootquestionary/controller/SurveyController.java
@@ -1,11 +1,16 @@
 package com.miranda.springboot.springbootquestionary.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.miranda.springboot.springbootquestionary.model.Question;
 import com.miranda.springboot.springbootquestionary.service.SurveyService;
@@ -21,6 +26,41 @@ public class SurveyController {
 		return surveyService.retrieveQuestions(surveyId);
 	}
 	
+	@PostMapping("/surveys/{surveyId}/questions")
+	public ResponseEntity<Void> addQuestionToSurvey(@PathVariable String surveyId,@RequestBody Question newQuestion){
+		/**
+		//What should be structure of request body?
+			{
+		        "id": "Question1",
+		        "description": "Largest Country in the World",
+		        "correctAnswer": "Russia",
+		        "options": [
+		            "India",
+		            "Russia",
+		            "United States",
+		            "China"
+		        ]
+		    }
+		//How will it be mapped to Question object? 
+		 * Using @RequestBody
+		//What should be returned?
+		//What should be response status?
+		 * 
+		 */
+		Question question = surveyService.addQuestion(surveyId, newQuestion);
+		
+		if(question == null)
+			return ResponseEntity.noContent().build();
+		//Success - URI of the new resource in Response Header
+		
+		 URI location = ServletUriComponentsBuilder.fromCurrentRequest().path(
+				"/{questionId}").buildAndExpand(question.getId()).toUri();
+		
+		//Status - Created
+		return ResponseEntity.created(location).build();
+	}
+	
+	             
 	@GetMapping("/surveys/{surveyId}/questions/{questionId}")
 	public Question retrieveDetailsForQuestion(@PathVariable String surveyId, @PathVariable String questionId){
 		return surveyService.retrieveQuestion(surveyId, questionId);
