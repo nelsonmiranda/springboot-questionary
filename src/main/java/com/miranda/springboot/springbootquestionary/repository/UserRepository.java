21581dfb8e0f4e6f47c844231c9d92fea78b566d
Nelson Miranda <nelsonmiranda8421@gmail.com>
2020-06-05 15:36:51 -0600

    Adding spring data rest

--- src/main/java/com/miranda/springboot/springbootquestionary/repository/UserRepository.java
+++ src/main/java/com/miranda/springboot/springbootquestionary/repository/UserRepository.java
@@ -3,11 +3,15 @@ package com.miranda.springboot.springbootquestionary.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.miranda.springboot.springbootquestionary.entity.User;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+@RepositoryRestResource(path = "users",collectionResourceRel = "users")
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 	
-	List<User> findByRole(String rol);
+	List<User> findByRole(@Param("role") String role);
 
 }
---
+++ src/main/java/com/miranda/springboot/springbootquestionary/repository/UserRestRepository.java
@@ -0,0 +1,13 @@
+package com.miranda.springboot.springbootquestionary.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.miranda.springboot.springbootquestionary.entity.User;
+
+public interface UserRestRepository extends CrudRepository<User, Long> {
+	
+	List<User> findByRole(String rol);
+
+}
